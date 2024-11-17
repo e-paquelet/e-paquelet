98763b366d07a78e6488dd39da282df90461e5a9
ShadowoftheRedFox <leon.merin@gmail.com>
2024-11-17 12:03:55 +0100

    deleted docs, fix

--- src/shared/header/header.component.ts
+++ src/shared/header/header.component.ts
@@ -1,53 +1,53 @@
 import { Component } from '@angular/core';
 
 export interface ListeMenu {
-  titre: string;
-  title?: string;
-  redirection: string,
+    titre: string;
+    title?: string;
+    redirection: string,
 }
 
 export interface ListeReseau {
-  img: string;
-  redirection: string,
-  alt:string
+    img: string;
+    redirection: string,
+    alt: string
 }
 
 @Component({
-  selector: 'app-header',
-  standalone: true,
-  imports: [],
-  templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+    selector: 'app-header',
+    standalone: true,
+    imports: [],
+    templateUrl: './header.component.html',
+    styleUrl: './header.component.css'
 })
 export class HeaderComponent {
-bgImgUrl1 = "./assets/img/favicon.png"
+    bgImgUrl1 = "./assets/img/favicon.png"
 
-  listeMenu: ListeMenu[] = [
-    {
-      titre: "Alternance",
-      redirection: "./entreprise"
-    },
-    {
-      titre: "Mes compétences",
-      redirection: "./portfolio",
-    },
-    {
-      titre: "Projet BUT",
-      redirection: "./projet-but",
-      title: "Les projets fait pour mon BUT"
-    },
-    {
-      titre: "Contact",
-      redirection: "./contact",
-    },
-  ];
+    listeMenu: ListeMenu[] = [
+        {
+            titre: "Alternance",
+            redirection: "./entreprise"
+        },
+        {
+            titre: "Mes compétences",
+            redirection: "./portfolio",
+        },
+        {
+            titre: "Projet BUT",
+            redirection: "./projet-but",
+            title: "Les projets fait pour mon BUT"
+        },
+        {
+            titre: "Contact",
+            redirection: "./contact",
+        },
+    ];
 
-  listeReseaux: ListeReseau[] = [
-    {
-      redirection: "https://www.linkedin.com/in/etienne-paquelet/",
-      img: "/assets/img/linkedin.png",
-      alt: "Linkedin"
-    },
-   
-  ]
+    listeReseaux: ListeReseau[] = [
+        {
+            redirection: "https://www.linkedin.com/in/etienne-paquelet/",
+            img: "./assets/img/linkedin.png",
+            alt: "Linkedin"
+        },
+
+    ]
 }
