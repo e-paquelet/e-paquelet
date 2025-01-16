1eb019bac55dd3083256f9a2d2cb43be7a15c56e
Kyrazail <etienne.paquelet@gmail.com>
2025-01-16 22:26:51 +0100

    minor change

--- src/app/interest/interest.component.ts
+++ src/app/interest/interest.component.ts
@@ -1,14 +1,22 @@
-import { Component } from '@angular/core';
+import { afterNextRender, Component, inject, Injector, ViewChild } from '@angular/core';
+import {MatInputModule} from '@angular/material/input';
+import {MatSelectModule} from '@angular/material/select';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {CdkTextareaAutosize, TextFieldModule} from '@angular/cdk/text-field';
 
 export interface ListeImage {
   titre: string;
   image: string;
+}
 
+export interface ListeImage2 {
+  titre: string;
+  image: string;
 }
 
 @Component({
   selector: 'app-interest',
-  imports: [],
+  imports: [MatFormFieldModule, MatSelectModule, MatInputModule, TextFieldModule],
   templateUrl: './interest.component.html',
   styleUrl: './interest.component.css'
 })
@@ -19,7 +27,12 @@ export class InterestComponent {
   autoadm = "./assets/img/interet/powershell.PNG";
   velo = "./assets/img/interet/bike.jpg";
   rock = "./assets/img/interet/rock.jpg";
-  pointinterro = "./assets/img/menu/pointinterro.jpg";
+  guitare = "./assets/img/interet/guitare.webp";
+  chess = "./assets/img/interet/chess.jpg";
+  gaming = "./assets/img/interet/gaming.jpg";
+  tennis = "./assets/img/interet/tennis.jpg";
+
+  valueSelected = "professionel";
 
   listeImage: ListeImage[] = [
     {
@@ -38,8 +51,39 @@ export class InterestComponent {
       titre: "L'automatisation des tâches d'administration",
       image: this.autoadm,
     },
+    {
+      titre: "La sécurisation du SI",
+      image: this.autoadm,
+    },
     
   ];
+
+  listeImage2: ListeImage2[] = [
+    {
+      titre: "Le tennis",
+      image: this.tennis,
+      
+    },
+    {
+      titre: "Les échecs",
+      image: this.chess,
+    },
+    {
+      titre: "la guitare",
+      image: this.guitare,
+    },
+    {
+      titre: "Le métal - rock",
+      image: this.rock,
+    },
+    {
+      titre: "Le vélo",
+      image: this.velo,
+    },
+    
+  ];
+
+  
 
   makeImgUrl(str: string): string {
     return "url('" + str + "')"
