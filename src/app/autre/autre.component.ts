ed236db77e9bf1c82623a65b12312896e96fda6d
Kyrazail <etienne.paquelet@gmail.com>
2025-01-26 16:07:32 +0100

    add

--- src/app/autre/autre.component.ts
+++ src/app/autre/autre.component.ts
@@ -9,4 +9,5 @@ import { VideoComponent } from '../../shared/video/video.component';
 })
 export class AutreComponent {
   bgImgUrl = "./assets/img/background2.mp4";
+  profil = "./assets/img/autre/moi à télécom.jpg"
 }
